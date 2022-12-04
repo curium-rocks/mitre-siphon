2d9d8aaa2f11d1082489d585f1ea299d438208f0
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-12-04 14:07:21 -0600

    fix security hotspot

--- src/main/java/rocks/curium/mitresiphon/controllers/RedirectController.java
+++ src/main/java/rocks/curium/mitresiphon/controllers/RedirectController.java
@@ -5,11 +5,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("redirectController")
 public class RedirectController {
-  @RequestMapping("/")
+  @GetMapping(path = "/")
   public void redirectToSwaggerUi(
       ModelMap model, HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.sendRedirect("/swagger-ui.html");
